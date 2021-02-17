f514dbdb6e42906482ffe7696a12180c8333d7cf
Sevan <sevanyeritsyan@gmail.com>
2021-02-17 20:52:22 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import "./App.css"
+// import ToDo from "./ToDo/ToDo"
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <h1>LBranch</h1>
+        <h2>Lesson Branch</h2>
+        <h3>Lesson Branch</h3>
+      </div>
+    )
+  }
+}
