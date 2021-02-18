07d356f69bc68c0faa8837e93df839fb78afdd2d
Sevan <sevanyeritsyan@gmail.com>
2021-02-18 12:44:46 +0400

    new

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react'
 import "./App.css"
-// import ToDo from "./ToDo/ToDo"
+
 
 export default class App extends Component {
   render() {
     return (
       <div className="wrapper">
-        <h1>LBranch</h1>
-        <h3>Lesson Branch</h3>
-        console.log("Test 2")
       </div>
     )
   }
