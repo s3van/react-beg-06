0e10f77f8695ea1387f36b465a95abb1a5ca8826
Sevan <sevanyeritsyan@gmail.com>
2021-02-18 13:01:00 +0400

    AddNewTask

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import "./App.css"
+import AddNewTask from "./Lesson1/AddNewTask"
 
 
 export default class App extends Component {
   render() {
     return (
       <div className="wrapper">
+        <AddNewTask />
       </div>
     )
   }
---
+++ src/Lesson1/AddNewTask.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class AddNewTask extends Component {
+    render() {
+        return (
+            <div>
+                <h1>AddNewTask</h1>
+                <h2>Git course</h2>
+            </div>
+        )
+    }
+}
+
+export default AddNewTask
