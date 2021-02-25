1fa637352e856612cfcc479aaab7248a5fb8f0a6
Sevan <sevanyeritsyan@gmail.com>
2021-02-25 13:54:30 +0400

    Lesson-8

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react'
+import './App.css'
 import Todo from './Components/Todo/Todo'
-import "./App.css"
-
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>
-          <h1><Todo /></h1>
-        </div>
+        <Todo />
       </div>
     )
   }
