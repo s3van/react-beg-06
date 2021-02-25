f130c79583b82f341167bd25f4be0a1bad40ed33
Sevan <sevanyeritsyan@gmail.com>
2021-02-25 22:49:16 +0400

    axs

--- src/Components/AddTask/AddTask.jsx
+++ src/Components/AddTask/AddTask.jsx
@@ -1,56 +1,53 @@
-import React, { Component } from 'react'
-import AddTaskStyles from './AddTask.module.css'
+import React, { Component } from "react";
+import AddTaskStyles from "./AddTask.module.css";
 
 export class AddTask extends Component {
+  state = {
+    inputValue: "",
+  };
 
-    state = {
-        inputValue: ""
-    }
+  handleChange = (event) => {
+    const { value } = event.target;
+    this.setState({
+      inputValue: value,
+    });
+  };
 
-    handleChange = (event) => {
-        const {value} = event.target
-        this.setState({
-            inputValue: value
-        })
-    }
-    
-    handleSubmit2 = () => {
-        if(!this.state.inputValue)
-        return
-        this.props.handleSubmit(this.state.inputValue)
-        this.setState({
-            inputValue: ""
-        })
-    }
+  handleSubmit2 = () => {
+    if (!this.state.inputValue) return;
+    this.props.handleSubmit(this.state.inputValue);
+    this.setState({
+      inputValue: "",
+    });
+  };
 
-    handleSubmit3 = (event) => {
-        if(event.keyCode === 13){
-        this.props.handleSubmit(this.state.inputValue)
-        this.setState({
-            inputValue: ""
-        })}
+  handleSubmit3 = (event) => {
+    if (this.state.inputValue && event.keyCode === 13) {
+      this.props.handleSubmit(this.state.inputValue);
+      this.setState({
+        inputValue: "",
+      });
     }
-    
+  };
 
-    render() {
-        return (
-            <div className={AddTaskStyles.wrapper}>
-                 <div>
-                    <input 
-                    type="text" 
-                    placeholder="Add Task"
-                    onChange={this.handleChange}
-                    onKeyUp={this.handleSubmit3}
-                    value={this.state.inputValue} 
-                    />
-                    <button 
-                    onClick={this.handleSubmit2}
-                    className={AddTaskStyles.btn}>
-                    Add</button>
-                </div> 
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className={AddTaskStyles.wrapper}>
+        <div className={AddTaskStyles.item}>
+          <input
+            type="text"
+            placeholder="Add Task"
+            onChange={this.handleChange}
+            onKeyUp={this.handleSubmit3}
+            value={this.state.inputValue}
+          />
+          <button onClick={this.handleSubmit2} className={AddTaskStyles.btn}>
+            Add
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default AddTask
+export default AddTask;
--- src/Components/Task/Task.jsx
+++ src/Components/Task/Task.jsx
@@ -1,15 +1,22 @@
-import TaskStyles from './Task.module.css'
+import TaskStyles from "./Task.module.css";
 
 const Task = (props) => {
-    const {task} = props
-    return (
-        <div className={TaskStyles.wrapper}>
-            <p className={TaskStyles.p}>{task}</p>
-            <button className={TaskStyles.btn1}>✓</button>
-            <button className={TaskStyles.btn2}>✗</button>
-            <div className={TaskStyles.info}><span className={TaskStyles.spanni}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem laudantium optio natus sunt expedita animi reiciendis assumenda dolore perferendis itaque</span></div>
-        </div>
-    )
-}
+  const { task } = props;
+  return (
+    <div className={TaskStyles.wrapper}>
+      <p className={TaskStyles.ptask}>{task}</p>
+      <button className={TaskStyles.btn1}>✓</button>
+      <button className={TaskStyles.btn2}>✗</button>
+      <div className={TaskStyles.info}>
+        <span className={TaskStyles.span1}>▲</span>
+        <span className={TaskStyles.span2}>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem
+          laudantium optio natus sunt expedita animi reiciendis assumenda dolore
+          perferendis itaque
+        </span>
+      </div>
+    </div>
+  );
+};
 
-export default Task
+export default Task;
