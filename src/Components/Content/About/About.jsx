00a31e00900663f667e477f05fb2a57dd56fbe89
Sevan <sevanyeritsyan@gmail.com>
2021-04-30 20:14:42 +0400

    new commit

--- src/Components/Content/About/About.jsx
+++ src/Components/Content/About/About.jsx
@@ -1,31 +1,51 @@
-import AboutStyles from "./About.module.css";
-
-const AboutUs = (props) => {
+import styles from "./About.module.css";
+import { Card } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+const About = () => {
   return (
-    <div className={AboutStyles.aboutWrap}>
-      <div className={AboutStyles.aboutTitle}>
-        <h1 style={{ fontSize: "42px" }}>About</h1>
-      </div>
-      <div className={AboutStyles.about}>
-        <div className={AboutStyles.myName}>
-        <div>Sevan Yeritsyan</div>
-        <div className={AboutStyles.myJob}>Junior React JS Developer</div>
-        </div>
-        
-        <div className={AboutStyles.me}>
-          I am 22 years old.Nuclear engineer by profession.Аlthough love my
-          profession and the sphere of nuclear energy,i didn't want to work in
-          this area.I saw myself in the IT,and follow this path.
-        </div>
-        <div className={AboutStyles.contactWithMe}>
-          <div className={AboutStyles.getWithMe}>Get in Touch with Me </div>
-          <div>Cell | +(374)-99-888-996 </div>
-         <div> Gmail | sevanyeritsyan@gmail.com</div>
-          <div>Portfolio | sevansportfolio01999.webflow.io</div>
-          <div>LinkedIn | https://www.linkedin.com/in/sevan-yeritsyan-aa2b5620b/</div>
-        </div>
+    <div className={styles.aboutHolder}>
+      <div>
+        <Card>
+          <Card.Body>
+            <Card.Title>Sevan Yeritsyan</Card.Title>
+            <Card.Text className={styles.userInfo}>
+              I have successfully completed an React/Redux educational programm
+              in Bitschool Business Development Group.
+            </Card.Text>
+            <hr className={styles.hr} />
+          </Card.Body>
+          <div className={styles.contactsUser}>
+            <div className={styles.contactUser}>
+              <FontAwesomeIcon icon={faEnvelope} />
+              <address className={styles.address}>
+                sevanyeritsyan@gmail.com
+              </address>
+            </div>
+            <div className={styles.contactUser}>
+              <FontAwesomeIcon icon={faPhone} />
+              <address className={styles.address}>+374-99-888-996</address>
+            </div>
+            <div className={styles.contactUser}>
+              <Card.Link href="https://github.com/s3van" target="_blank">
+                <FontAwesomeIcon icon={faGithub} />
+              </Card.Link>
+              <address className={styles.address}>Github</address>
+            </div>
+            <div className={styles.contactUser}>
+              <Card.Link
+                href="https://www.linkedin.com/in/gev-margaryan-b5a571188/"
+                target="_blank"
+              >
+                <FontAwesomeIcon icon={faLinkedin} />
+              </Card.Link>
+              <address className={styles.address}>LinkendIn</address>
+            </div>
+          </div>
+        </Card>
       </div>
     </div>
   );
 };
-export default AboutUs;
+export default About;
